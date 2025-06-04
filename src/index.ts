3fcd181d23d75874fcae3a558aa9c5907f1051cd
alex-reinfoce <alex.lichangnan@gmail.com>
2025-06-04 08:34:03 +0800

    Refactor SmoothScrollController: update constructor to accept options object, enhance container handling, and improve scroll event management in index.ts. Adjust instantiation in test.html accordingly.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,51 @@
+interface SmoothScrollControllerOptions {
+  container: HTMLElement;
+}
+
 export default class SmoothScrollController {
-  constructor(private container: HTMLElement) {
-    this.container = container;
+  private options: SmoothScrollControllerOptions;
+
+  private get container() {
+    return this.options.container;
   }
+
+  private autoScroll = true;
+
+  private lastScrollHeight = 0;
+
+  private onScroll(e: Event) {
+
+  }
+
+  private bundleEvent() {
+    this.container.addEventListener('scroll', this.onScroll)
+  }
+
+  constructor(options: SmoothScrollControllerOptions) {
+    this.options = options;
+
+    if (!this.options.container) {
+      throw new Error('container is required')
+    }
+
+    this.bundleEvent()
+  }
+
   public settleDown() {
-    console.log(1)
+    if (!this.autoScroll) {
+      return
+    }
+
+    if (this.lastScrollHeight > 0) {
+      console.log(this.container.scrollHeight)
+    } else {
+      window.requestAnimationFrame(() => {
+        this.lastScrollHeight = this.container.scrollHeight;
+        this.container.scrollTo({
+          top: this.container.scrollHeight,
+          behavior: 'smooth'
+        })
+      })
+    }
   }
-} 
+}
